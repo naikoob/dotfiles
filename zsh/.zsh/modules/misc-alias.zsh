cba88cd12e5fa2735a6ef872ab228c668ebd160e
naikoob <bookian@gmail.com>
2020-08-04 09:49:35 +0800

    fixed condition check

--- zsh/.zsh/modules/misc-alias.zsh
+++ zsh/.zsh/modules/misc-alias.zsh
@@ -1,18 +1,18 @@
 # Aliases
-alias ls='ls -FG'
+alias ls='ls -F --color=auto'
 alias ll='ls -lh'
 alias la='ls -a'
 alias df='df -H'
 
 # use nvim as vim if available
-if [ type nvim >& /dev/null ]; then
+if type nvim >& /dev/null; then
     alias vi='nvim'
     alias vim='nvim'
     alias oldvim='\vim'
 fi
 
 # to have similar behavior
-if [ type vscode >& /dev/null ]; then
+if type vscode >& /dev/null; then
     alias code='vscode'
 fi
 
