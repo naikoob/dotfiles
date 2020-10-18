c1749a164fd60fa9a6619fc5f328655ff3dae6cc
naikoob <bookian@gmail.com>
2020-10-18 22:37:58 +0800

    bunch of small changes

--- zsh/.zsh/01-environment.zsh
+++ zsh/.zsh/01-environment.zsh
@@ -15,3 +15,12 @@ export PAGER='less'
 export LC_CTYPE=en_US.UTF-8
 export LANG=en_US.UTF-8
 
+# fcitx
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+
+# qt5
+export QT_SCALE_FACTOR=1.6
+export QT_STYLE_OVERRIDE=adwaita
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,2 +1,7 @@
 # load configs
 for config (~/.zsh/*.zsh) source $config
+
+# start sway if I'm on the console
+if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+    exec sway
+fi
