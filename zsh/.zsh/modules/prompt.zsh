94fcc43f241c836be207a308ac753414f0ab068c
naikoob <bookian@gmail.com>
2022-06-30 11:27:01 +0800

    fixed python sitepackage location

--- zsh/.zsh/modules/prompt.zsh
+++ zsh/.zsh/modules/prompt.zsh
@@ -1,4 +1,4 @@
 # use powerline prompt if available
 if (( $+commands[powerline] )); then
-  . /usr/lib/python3.9/site-packages/powerline/bindings/zsh/powerline.zsh
+  . /usr/lib/python/site-packages/powerline/bindings/zsh/powerline.zsh
 fi
