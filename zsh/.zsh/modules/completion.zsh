7c409cfb21a3b373ebc06a133f059d582e29503a
naikoob <bookian@gmail.com>
2022-07-26 09:51:21 +0800

    add zsh completion

--- zsh/.zsh/modules/completion.zsh
+++ zsh/.zsh/modules/completion.zsh
@@ -1,8 +1,8 @@
 # autocompletion systems
 fpath=($HOME/.zsh/completion $fpath)
 
-autoload -Uz compinit
-compinit -u
+autoload -Uz compinit && compinit -u 
+autoload -U +X bashcompinit && bashcompinit
 
 # case insensitive path-completion
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
